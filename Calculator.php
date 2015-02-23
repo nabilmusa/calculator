70abbe0896369fa0c243e98d8b779979d245f703
nabilmusa <nbl_musa@yahoo.com>
2015-02-23 09:26:26 +0400

    Revert "Update Calculator.php"

--- Calculator.php
+++ Calculator.php
@@ -4,11 +4,11 @@ class Calculator {
 
 
 	public function add($num1, $num2){
-		return $num1 * $num2;
+		return $num1 + $num2;
 	}
 
 	public function subtract($num1, $num2){
-		return $num1 + $num2;
+		return $num1 - $num2;
 	}
 
 	public function divide($num1, $num2){
